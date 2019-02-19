efae9bbb6188cd2d7e96430657de3c9b91bece9b
Mike <36168675+Mike76b@users.noreply.github.com>
2019-02-19 11:39:05 +0100

    Update selection_sort.c

--- selection_sort.c
+++ selection_sort.c
@@ -16,7 +16,7 @@ int main(void)
     // Array length.
     int n = sizeof(values) / sizeof(values[0]);
 
-        // Sorting loop.
+    // Sorting loop.
     for (int i = 0; i < (n - 1); i++)
     {
         int min = values[i];
